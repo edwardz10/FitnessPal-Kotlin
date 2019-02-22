322375cff70112394dfaf7ad50e2efb765af9976
Eduard Ziiangulov z003xzbz <eduard.ziiangulov@siemens.com>
2019-02-22 18:06:02 +0300

    - some data is read successfully from an Excel.

--- app/src/main/java/com/bignerdranch/android/fitnesspal/db/dml/ExcelDataLoader.kt
+++ app/src/main/java/com/bignerdranch/android/fitnesspal/db/dml/ExcelDataLoader.kt
@@ -5,8 +5,11 @@ import android.database.sqlite.SQLiteDatabase
 import android.util.Log
 import com.bignerdranch.android.fitnesspal.MainActivity
 import com.bignerdranch.android.fitnesspal.db.DbConstants
+import com.bignerdranch.android.fitnesspal.model.Measurement
+import com.bignerdranch.android.fitnesspal.model.TrainingSessionType
 import org.apache.poi.hssf.usermodel.HSSFWorkbook
 import org.apache.poi.ss.usermodel.Workbook
+import java.util.*
 
 class ExcelDataLoader(database: SQLiteDatabase) : AbstractDataLoader(database) {
 
@@ -20,22 +23,37 @@ class ExcelDataLoader(database: SQLiteDatabase) : AbstractDataLoader(database) {
         Log.i(DbConstants.DATABASE_NAME, FITNESS_REPORT_FILE + " successfully loaded")
     }
 
-    override val trainingSessionTypes: List<ContentValues>
-        get() = emptyList()
+    override val measurements: List<ContentValues> by lazy {
+        Arrays.asList(
+            Measurement.getContentValues(Measurement(DataConstants.KILOGRAMS)),
+            Measurement.getContentValues(Measurement(DataConstants.METERS)),
+            Measurement.getContentValues(Measurement(DataConstants.SECONDS)),
+            Measurement.getContentValues(Measurement(DataConstants.TIMES))
+        )
+    }
 
-    override val sets: List<ContentValues>
-        get() = emptyList()
+    override val trainingSessionTypes: List<ContentValues> by lazy {
+        val typesLocal = LinkedList<ContentValues>()
 
-    override val trainingSessions: List<ContentValues>
+        for (i in 0 .. workbook.numberOfSheets - 1) {
+            typesLocal.add(TrainingSessionType.getContentValues(TrainingSessionType(workbook.getSheetAt(i).sheetName)))
+        }
+
+        typesLocal
+    }
+
+    override val sets: List<ContentValues> by lazy {
+        val setsLocal = LinkedList<ContentValues>()
+        setsLocal
+    }
+
+    override val exercises: List<ContentValues>
         get() = emptyList()
 
     override val reps: List<ContentValues>
         get() = emptyList()
 
-    override val exercises: List<ContentValues>
-        get() = emptyList()
-
-    override val measurements: List<ContentValues>
+    override val trainingSessions: List<ContentValues>
         get() = emptyList()
 
 
